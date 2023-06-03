b16455ca5d2d8ae7077f9c9cfa2082c85252a84b
hiroki_mori3 <porizou.electro@gmail.com>
2023-06-04 07:11:09 +0900

    update

--- launch/ndt_scan_matcher.launch.py
+++ launch/ndt_scan_matcher.launch.py
@@ -1,32 +1,43 @@
 from launch import LaunchDescription
-from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.launch_description_sources import AnyLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
     # Declare the launch arguments
     pointcloud_map_path_arg = DeclareLaunchArgument(
         'pointcloud_map_path',
         default_value='/home/porizou/autoware_map/sample-map-rosbag/pointcloud_map.pcd',
-        description='Path to the pointcloud map')
+        description='Path to the pointcloud map'
+    )
+
+    livox_ros_driver2_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [FindPackageShare('livox_ros_driver2'), '/launch/rviz_MID360_launch.py']
+        )
+    )
+
+    ndt_scan_matcher_launch = IncludeLaunchDescription(
+        AnyLaunchDescriptionSource(
+            [FindPackageShare('ndt_scan_matcher'), '/launch/ndt_scan_matcher.launch.xml']
+        )
+    )
+
+    map_loader_launch = IncludeLaunchDescription(
+        AnyLaunchDescriptionSource(
+            [FindPackageShare('map_loader'), '/launch/pointcloud_map_loader.launch.xml']
+        ),
+        launch_arguments={'pointcloud_map_path': LaunchConfiguration('pointcloud_map_path')}.items()
+    )
 
     return LaunchDescription([
         # Launch arguments
         pointcloud_map_path_arg,
-        Node(
-            package='livox_ros_driver2',
-            executable='msg_MID360',
-            output='screen',
-        ),
-        Node(
-            package='autoware_auto_algorithm',
-            executable='ndt_scan_matcher',
-            output='screen',
-        ),
-        Node(
-            package='map_loader',
-            executable='pointcloud_map_loader',
-            output='screen',
-            parameters=[{'pointcloud_map_path': LaunchConfiguration('pointcloud_map_path')}]
-        ),
+        # Launch configurations
+        livox_ros_driver2_launch,
+        ndt_scan_matcher_launch,
+        map_loader_launch
     ])
